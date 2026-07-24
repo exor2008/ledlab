0d1799437ff4a40d3671e6bdcfc002cf88f56f63
exor2008 <exorsteam2008@gmail.com>
2026-07-24 00:15:35 -0400

    Feature: Command handler

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,13 @@
 #![no_std]
 #![no_main]
 
+use crate::{
+    buffer::Buffer,
+    world::{GetTicker, GetWorld, OnDirection, Switch, Tick},
+};
 use embassy_rp::{
     pio::Instance,
     pio_programs::ws2812::{Grb, PioWs2812},
-};
-
-use crate::{
-    buffer::Buffer,
-    utils::Direction,
-    world::{GetTicker, GetWorld, OnDirection, Switch, Tick},
 };
 
 pub mod apds9960;
@@ -20,20 +18,32 @@ pub mod perlin;
 pub mod utils;
 pub mod world;
 
-async fn start<'d, Coord, B, W, P, const WORLDS: usize, const S: usize, const N: usize>(
+pub trait CommandHandler<W, Coord, B, const N: usize, const WORLDS: usize>
+where
+    W: Tick<Coord, B, N> + GetTicker + GetWorld + OnDirection,
+    B: Buffer<Coord, N>,
+{
+    fn handle(&self, buffer: &mut B, world: &mut W, switch: &mut Switch<WORLDS>);
+}
+
+pub async fn start<'d, Coord, B, W, H, P, const WORLDS: usize, const S: usize, const N: usize>(
     buffer: &mut B,
     switch: &mut Switch<WORLDS>,
     ws2812: &mut PioWs2812<'d, P, S, N, Grb>,
+    handler: H,
 ) where
     B: Buffer<Coord, N>,
     W: Tick<Coord, B, N> + GetTicker + GetWorld + OnDirection,
+    H: CommandHandler<W, Coord, B, N, WORLDS>,
     P: Instance,
 {
     let mut world = W::get_world(1);
 
-    world.on_direction(Direction::Up);
-    world = switch.switch_world();
-    world.tick(buffer);
-    ws2812.write(&buffer.data()).await;
-    world.get_ticker().next().await;
+    loop {
+        handler.handle(buffer, &mut world, switch);
+
+        world.tick(buffer);
+        ws2812.write(&buffer.data()).await;
+        world.get_ticker().next().await;
+    }
 }
